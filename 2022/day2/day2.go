62f67fe4ab2b97a0f6d227138e6d78253becdeab
Florian Rädiker <github@florian-raediker.de>
2022-12-02 10:18:53 +0100

    Add day2

---
+++ 2022/day2/day2.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"aoc2022/util"
+	"strings"
+)
+
+func main() {
+	util.Run(2022, 2, parseInput, part1, part2)
+}
+
+func parseInput(input string) []string {
+	return strings.Split(input, "\n")
+}
+
+// A/X defeats C/Z
+// B/Y defeats A/X
+// C/Z defeats B/Y
+
+var GAMES = map[string]struct {
+	part1 int
+	part2 int
+}{
+	"A X": {1 + 3, 0 + 3},
+	"A Y": {2 + 6, 3 + 1},
+	"A Z": {3 + 0, 6 + 2},
+
+	"B X": {1 + 0, 0 + 1},
+	"B Y": {2 + 3, 3 + 2},
+	"B Z": {3 + 6, 6 + 3},
+
+	"C X": {1 + 6, 0 + 2},
+	"C Y": {2 + 0, 3 + 3},
+	"C Z": {3 + 3, 6 + 1},
+}
+
+func part1(input []string) (score int) {
+	for _, round := range input {
+		score += GAMES[round].part1
+	}
+	return
+}
+
+func part2(input []string) (score int) {
+	for _, round := range input {
+		score += GAMES[round].part2
+	}
+	return
+}
